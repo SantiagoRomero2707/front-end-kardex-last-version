f27933e48c5108c736598efb6009a6e6e63a2973
Santiago Romero <santyago.2000@hotmail.com>
2023-01-05 10:48:00 -0500

    Arreglar los microservicios, falta el de kardex

--- kardex-last-version/src/app/models/crud/product.ts
+++ kardex-last-version/src/app/models/crud/product.ts
@@ -5,11 +5,10 @@ export class Product{
     nitProducto!: string;
     proveedor!: string;
 
-    constructor(nombreProducto:string, precioUnitario:number,nitProducto:string,proveedor:string){
+    constructor(nombreProducto:string, precioUnitario:number, nitProducto:string, proveedor:string){
         this.nombreProducto = nombreProducto,
         this.precioUnitario = precioUnitario;
         this.nitProducto = nitProducto;
         this.proveedor = proveedor;
-    }
-    
+    }   
 }
